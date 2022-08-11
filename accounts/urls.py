1030d6615f327d9e3aaf4da62d2a90ab726c9e5a
Manuel Danieli <mdanieli@outlook.com>
2022-08-11 10:20:15 -0300

    agregamos login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from importlib.resources import path
+from django.urls import path
 from .views import login
 
 urlpatterns = [
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,23 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as django_login
 def login(request):
-    return HTTPResponse('')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                django_login(request, user)
+                redirect('Home')
+            else:
+                return render(request, 'accounts/login.html', {'form': form})
+        else:
+            return render(request, 'accounts/login.html', {'form': form})
+            
+            
+    form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
