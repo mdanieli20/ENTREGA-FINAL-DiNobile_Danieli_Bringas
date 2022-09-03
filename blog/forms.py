69802b33f500e150c33d503f7d62f46a1eb2708a
mdanieli <mdanieli@outlook.com>
2022-09-03 15:28:18 -0300

    agregamos ckeditorWidget al formulario de creacion de noticia

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,10 @@
 from unittest.util import _MAX_LENGTH
 from django import forms
+from ckeditor.widgets import CKEditorWidget
 
 class FormNoticia(forms.Form):
     titulo = forms.CharField(max_length=30)
-    contenido = forms.CharField(widget=forms.Textarea)
+    contenido = forms.CharField(widget=CKEditorWidget())
     fecha_creacion = forms.DateField(required=False)
     imagen = forms.ImageField(required=False)
     
